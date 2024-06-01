817b9fa694a2696ddb393f83f95a3a063f2151f5
mayyashar <may27499@gmail.com>
2024-06-01 17:15:34 +0300

    firstcommit

--- mayyshar.java
+++
@@ -1,8 +0,0 @@
-public class mayyashar {
-    public static void main(String[] args) {
-        int times = 5;
-        for (int i = 0; i < times; i++) {
-            System.out.println("may yashar");
-        }
-    }
-}
